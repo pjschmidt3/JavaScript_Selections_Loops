75b056141a8405e541ca762fa9c392e0d03057a2
Phillip Schmidt <pjschmidt3@outlook.com>
2021-12-06 14:30:13 -0600

    update repo

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-console.log("Hello World!\n==========\n");
+// Exercise 1
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 1) {
+    console.log(i);
+  }
+}
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+// Exercise 2
+for (let i = 1; i <= 100; i++) {
+  let result = '';
 
-// Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+  if (i % 3 === 0) {
+    result += 'FIZZ';
+  }
+
+  if (i % 5 === 0) {
+    result += 'BUZZ';
+  }
+
+  if (result !== '') {
+    console.log(result);
+  }
+}
